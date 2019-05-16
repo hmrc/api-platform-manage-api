7d75a7b5097bf458874a19ddfc1135105febbf4e
Luis Burgos <1550179+lburgos@users.noreply.github.com>
2019-05-16 15:50:03 +0100

    API-3797: extra logging

--- src/main/scala/uk/gov/hmrc/api_platform_manage_api/AwsIdRetriever.scala
+++ src/main/scala/uk/gov/hmrc/api_platform_manage_api/AwsIdRetriever.scala
@@ -16,6 +16,7 @@
 
 package uk.gov.hmrc.api_platform_manage_api
 
+import com.amazonaws.services.lambda.runtime.LambdaLogger
 import software.amazon.awssdk.services.apigateway.ApiGatewayClient
 import software.amazon.awssdk.services.apigateway.model.{GetApiKeysRequest, GetRestApisRequest, GetRestApisResponse, GetUsagePlansRequest}
 
@@ -27,12 +28,12 @@ trait AwsIdRetriever {
   val apiGatewayClient: ApiGatewayClient
   val Limit: Int = 100
 
-  def getAwsRestApiIdByApiName(apiName: String): Option[String] = {
-    findAwsRestApiId(apiName, None)
+  def getAwsRestApiIdByApiName(apiName: String, logger: LambdaLogger): Option[String] = {
+    findAwsRestApiId(apiName, None, logger)
   }
 
   @tailrec
-  private def findAwsRestApiId(apiName: String, position: Option[String]): Option[String] = {
+  private def findAwsRestApiId(apiName: String, position: Option[String], logger: LambdaLogger): Option[String] = {
     def buildRequest(position: Option[String]): GetRestApisRequest = {
       position match {
         case Some(p) => GetRestApisRequest.builder().limit(Limit).position(p).build()
@@ -40,11 +41,13 @@ trait AwsIdRetriever {
       }
     }
 
+    logger.log(s"Getting APIs with position $position")
     val response: GetRestApisResponse = apiGatewayClient.getRestApis(buildRequest(position))
+    logger.log(s"Got ${response.items().size()} APIs")
 
     response.items().asScala.find(restApi => restApi.name == apiName) match {
       case Some(restApi) => Some(restApi.id)
-      case _ => if (response.items.size < Limit) None else findAwsRestApiId(apiName, Some(response.position))
+      case _ => if (response.items.size < Limit) None else findAwsRestApiId(apiName, Some(response.position), logger)
     }
   }
 
